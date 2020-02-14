499902060990ab5db16d858e199c5715fee3a606
Luis Costa <luiscapelacosta@gmail.com>
2020-02-14 15:27:36 +0000

    small changes

--- utils/func.php
+++ utils/func.php
@@ -1,9 +1,23 @@
 <?php
 
-define('PATH_STRUCTURES', '../utils/structures/' );
-define("UTILITY_PATH",'../utils/');
+define('PATH_STRUCTURES', './utils/structures/' );
 define('UTILITY_SEARCH_PATH', './utils/*/');
 define('INTERFACES_PATH', './interfaces/');
+
+
+
+
+function load_utilities()
+{
+    $files = glob(UTILITY_SEARCH_PATH . "*.php");
+    if(!empty($files))
+    {
+        foreach($files as $f)
+        {
+            include_once $f;
+        }
+    }
+}
 
 function load_interfaces()
 {
@@ -17,10 +31,8 @@ function load_interfaces()
     }
 }
 
-
 function load_utility($name)
 {
-    
     $files = glob(UTILITY_SEARCH_PATH . "$name.php");
     if(!empty($files))
     {
